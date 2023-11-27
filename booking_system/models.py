ebaaf03435c3684e572ad324df7e73aebc3914b5
Butros Groot MacBook <butrosgroot@gmail.com>
2023-11-27 11:57:05 +0100

    Project Name: NatureTech Commit Type: UUIDV7 + model
    Added booking model
    
    - Added the uuidv7 function
    - Changed the booking_model
    
    Signed-off-by: ButrosGroot <butrosgroot@gmail.com>

--- booking_system/models.py
+++ booking_system/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
-# Create your models here.
-class booking(models.Model):
-    Identity = ()
-    OrderNummer = models.IntegerField()
-    StartDate = models.DateField(auto_now=False, auto_now_add=False) 
-    EndDate = models.DateField(auto_now=False, auto_now_add=False)
-    TimeofBooking = models.DateField()
-    AgeBelow = models.IntegerField()
-    AgeAbove = models.IntegerField()
-    PDF = models.CharField(max_length=255)
-    CheckedIn = models.BooleanField()
-    Payed = models.BooleanField()
+from NatureTech.programs.uuidv7 import uuid7
+
+class Booking(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid7, editable=False)
+    order_number = models.IntegerField()
+    start_date = models.DateField(auto_now=False, auto_now_add=False) 
+    end_date = models.DateField(auto_now=False, auto_now_add=False)
+    time_of_booking = models.DateTimeField(auto_now_add=True)
+    age_below = models.IntegerField()
+    age_above = models.IntegerField()
+    pdf = models.CharField(max_length=255, blank=True, null=True)
+    checked_in = models.BooleanField(default=False)
+    paid = models.BooleanField(default=False)
     # CampingSpot = models.ForeignKey()
     # ExtraServices = models.ForeignKey()
     # Customer = models.ForeignKey()

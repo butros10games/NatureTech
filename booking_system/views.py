64f84f4c16ff24fb3574fb0a4444d947f5848f03
BobElsendoorn <bob.elsendoorn@student.hu.nl>
2023-11-28 12:21:01 +0100

    Project Name: NatureTech Commit Type: Commit
    Bug fixes + new features
    
    Signed-off-by: Bob Elsendoorn <Bobelsendoorn@gmail.com>

--- booking_system/views.py
+++ booking_system/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
+from .models import Booking, Customer
+from django.contrib.auth.models import User
 
 
 def booking_form(request):
@@ -11,6 +13,11 @@ def booking_form(request):
         start = request.POST['StartDate']
         end = request.POST['EndDate']
 
+        user, created = User.objects.get_or_create(first_name=first_name, last_name=last_name, email=mail)
+        customer, created = Customer.objects.get_or_create(user=user)
+        booking, created = Booking.objects.get_or_create(customer=customer, age_above=number_of_guests, start_date=start, end_date=end)
+
+        
         send_mail(
               'Boekings bevestiging',
             f'Dankuwel, {first_name} {last_name}, voor het reserveren van een kampeerplek op de boerencamping voor {number_of_guests} personen van {start} tot {end}.',

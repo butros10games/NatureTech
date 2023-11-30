6c085476ac08e173bff457131e090190fc3a3ffd
joosterws <joost.w.sluijmer@gmail.com>
2023-11-30 10:36:28 +0100

    Project Name: [NatureTech] Commit Type: Fix
    Changing BT and BLE code
    
    Signed-off-by: Joost Sluijmer <Joost.w.sluijmer@gmail.com>

--- RPi/BT_scan.py
+++ RPi/BT_scan.py
@@ -5,8 +5,8 @@ def scan_bluetooth_devices():
     print("Scanning for Bluetooth devices:")
     nearby_devices = bluetooth.discover_devices(duration=8, lookup_names=True, device_id=-1)
     print("Bluetooth devices found:")
-    for addr, name in nearby_devices:
-        print(f"  {name} ({addr}) - RSSI: {rssi}")
+    for nearby_devices in nearby_devices:
+        print(f" nearby_devices {nearby_devices}")
 
 if __name__ == "__main__":
     scan_bluetooth_devices()

9c2431a0c29fbe651feb64d7c5c87d275b905481
Bob Elsendoorn <bobelsendoorn@gmail.com>
2023-11-30 11:26:46 +0100

    Project Name: NatureTech Commit Type: Commit
    Contactform backend
    
    Signed-off-by: Bob Elsendoorn <bobelsendoorn@gmail.com>

--- booking_system/urls.py
+++ booking_system/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import booking_form, confirm_booking
+from .views import booking_form, confirm_booking, contact_form
 
 urlpatterns = [
     path('booking_form', booking_form, name='booking_form'),
     path('confirmation', confirm_booking, name='confirm_booking'),
+    path('contact_form', contact_form, name='contact_form')
 ]

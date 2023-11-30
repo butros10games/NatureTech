49b88995aa63bc9180e42dfeb80518aa4fc6f7ba
Butros Groot Desktop <butrosgroot@gmail.com>
2023-11-30 12:02:41 +0100

    Project Name: NatureTech Commit Type: Bufix
    Fixed a problem with the csrf and the routing of the contact request
    
    Signed-off-by: Butros Groot <Butrosgroot@gmail.com>

--- booking_system/urls.py
+++ booking_system/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import booking_form, confirm_booking, contact_form
+from .views import booking_form, confirm_booking
 
 urlpatterns = [
     path('booking_form', booking_form, name='booking_form'),
-    path('confirmation', confirm_booking, name='confirm_booking'),
-    path('contact_form', contact_form, name='contact_form')
+    path('confirmation', confirm_booking, name='confirm_booking')
 ]
--- customer_general/views.py
+++ customer_general/views.py
@@ -1,8 +1,41 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 # Create your views here.
 def index(request):
     return render(request, 'customer_general/index.html')
 
 def contact(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        mail = request.POST['email']
+        phone_number = request.POST['phone_number']
+        contact_message = request.POST['contact_message']
+
+        context = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": mail,
+            "phone_number": phone_number,
+            "contact_message": contact_message
+        }
+    
+        html_message = render_to_string("mail/contact_confirmation_mail.html", context=context)
+        plain_message = strip_tags(html_message)
+
+        message = EmailMultiAlternatives(
+            subject = 'Uw vraag is ontvangen', 
+            body = plain_message,
+            from_email = None ,
+            to= {mail}
+        )
+        message.attach_alternative(html_message, "text/html")
+        message.send()
+
+        messages.success(request, 'Uw vraag is verstuurd!')
+
     return render(request, 'customer_general/contact.html')

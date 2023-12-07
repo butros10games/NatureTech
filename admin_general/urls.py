20430c281466b927aef30b6ff5ce67aa4b67194b
Bob Elsendoorn <bobelsendoorn@gmail.com>
2023-12-07 11:34:08 +0100

    Project Name: NatureTech Commit Type: Commit
    Added the admin panel
    
    Signed-off-by: Bob Elsendoorn <bobelsendoorn@gmail.com>

--- admin_general/urls.py
+++ admin_general/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import admin_index
+from .views import admin_index, booking_context
 
 urlpatterns = [
     path('', admin_index, name='booking_index'),
+    path('booking_context', booking_context, name='booking_context')
 ]
--- admin_general/views.py
+++ admin_general/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from booking_system.models import Booking, Customer
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def admin_index(request):
-    return render(request, 'boer-admin/admin_general/admin_index.html')
+    return booking_context(request)
+
+def booking_context(request):
+    bookings = Booking.objects.all().order_by('id')
+
+    # Pass the bookings data to the template
+    context = {
+        'bookings': bookings,
+    }
+
+    return render(request, 'boer-admin/admin_general/admin_index.html', context)
+

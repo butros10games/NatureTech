fb039da3065af168c69287deefd885a6766b94f2
joosterws <joost.w.sluijmer@gmail.com>
2023-12-07 12:33:09 +0100

    Project Name: [NatureTech] Commit Type: Feature
    Script to test buttons/sensors like the door etc.
    
    [Optional longer description of the change, if needed]
    
    [Additional context or notes, such as related issues or other details]
    
    Signed-off-by: Joost Sluijmer <Joost.w.sluijmer@gmail.com>

--- RPi/Button_push.py
+++ RPi/Button_push.py
@@ -1,4 +1,38 @@
-from gpiozero import Button
-button = Button(2)
-button.wait_for_press()
-print('You pushed me')
+import RPi.GPIO as GPIO
+import time
+
+# Set the GPIO mode to BCM
+GPIO.setmode(GPIO.BCM)
+
+# Define the GPIO pin connected to the button
+button_pin = 17  # Change this to the GPIO pin you have connected the button to
+
+# Setup the button pin as an input with a pull-up resistor
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Initialize the variable to store the previous state of the button
+prev_button_state = GPIO.input(button_pin)
+
+try:
+    while True:
+        # Read the current state of the button
+        button_state = GPIO.input(button_pin)
+
+        # Check if the button state has changed
+        if button_state != prev_button_state:
+            if button_state == GPIO.LOW:
+                print("Button is pressed (closed)")
+            else:
+                print("Button is released (open)")
+
+            # Update the previous button state
+            prev_button_state = button_state
+
+        # Add a small delay to debounce the button
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print("Program terminated by user.")
+finally:
+    # Cleanup GPIO settings
+    GPIO.cleanup()

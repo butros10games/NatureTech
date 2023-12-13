f1af122955121083222e91fc6000263d5d5049dd
BobElsendoorn <bob.elsendoorn@student.hu.nl>
2023-12-13 10:50:21 +0100

    Project Name: NatureTech Commit Type: [Type of Commit]
    Een gebeunhaasde versie van pagination
    
    Signed-off-by: Bob Elsendoorn <Bobelsendoorn@gmail.com>

--- admin_general/views.py
+++ admin_general/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from booking_system.models import Booking, Customer
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import JsonResponse, HttpResponse
+from django.template.loader import render_to_string
+
 
 
 # Create your views here.
@@ -13,22 +16,21 @@ def admin_index(request):
     return render(request, 'boer-admin/admin_general/admin_index.html')
 
 def booking_context(request):
-    ## if the user is not logged in, redirect to the login page
     if not request.user.is_authenticated:
         return redirect('login')
 
     bookings = Booking.objects.all().order_by('id')
-    paginator = Paginator(bookings, 25)
+    paginator = Paginator(bookings, 10)
 
     page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
- 
-    # Pass the bookings data to the template
-    context = {
-        'bookings': bookings,
-        'page_obj': page_obj,
-    }
+    try:
+        page_obj = paginator.page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
 
-    return render(request, 'boer-admin/admin_general/admin_orders.html', context)
+    # Render the table rows as HTML
+    return render(request, 'boer-admin/admin_general/admin_orders.html', {'page_obj': page_obj})
 
 

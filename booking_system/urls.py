18c3c7e46b6fba6469f47af5a04a22594594b4fb
Butros Groot MacBook <butrosgroot@gmail.com>
2023-12-18 08:55:08 +0100

    Project Name: NatureTech Commit Type: Enhacement
    Added availablity check
    
    Signed-off-by: ButrosGroot <butrosgroot@gmail.com>

--- booking_system/urls.py
+++ booking_system/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import booking_form, confirm_booking, booking_index
+from .views import booking_form, confirm_booking, booking_index, beschikbaarheidsCheck
 
 urlpatterns = [
     path('', booking_index, name='booking_index'),
     path('booking_form/', booking_form, name='booking_form'),
     path('confirmation/', confirm_booking, name='confirm_booking'),
-    
+    path('beschikbaarheidsCheck/', beschikbaarheidsCheck, name='beschikbaarheidsCheck'),
 ]

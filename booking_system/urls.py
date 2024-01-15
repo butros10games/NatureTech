babe65aea6a322fdb30caca2b5731d2752572561
Butros Groot MacBook <butrosgroot@gmail.com>
2024-01-15 09:36:30 +0100

    Project Name: NatureTech Commit Type: Enhancement
    - url change
    
    Signed-off-by: ButrosGroot <butrosgroot@gmail.com>

--- booking_system/urls.py
+++ booking_system/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import booking_form, confirm_booking, booking_index, beschikbaarheidsCheck
+from .views import booking_form, confirm_booking, booking_index, beschikbaarheidsCheck, get_full_price
 
 urlpatterns = [
     path('', booking_index, name='booking_index'),
     path('booking_form/', booking_form, name='booking_form'),
     path('confirmation/', confirm_booking, name='confirm_booking'),
     path('beschikbaarheidsCheck/', beschikbaarheidsCheck, name='beschikbaarheidsCheck'),
+    path('api/total_price/', get_full_price, name='get_full_price')
 ]

f055ec2a670dde9e36a3302b3b5a64ae715060af
Butros Groot MacBook <butrosgroot@gmail.com>
2024-01-15 09:18:21 +0100

    Project Name: NatureTech Commit Type: Enhacement
    - GPS save
    
    Signed-off-by: ButrosGroot <butrosgroot@gmail.com>

--- gps_mapping/views.py
+++ gps_mapping/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import traceback
 
+from django.middleware.csrf import get_token
+
+from booking_system.models import Veld, VeldGps
+
 # Create your views here.
 def gps_index(request):
     return render(request, 'boer-admin/gps_mapping/gps_index.html')
 
 def gps_scan(request):
-    return render(request, 'boer-admin/gps_mapping/gps_scan.html')
+    context = {
+        'csrf_token': get_token(request),
+    }
+    return render(request, 'boer-admin/gps_mapping/gps_scan.html', context)
 
 # Gets a post request with all the gps data points and saves them to the database
 def gps_scan_save(request):
@@ -23,3 +30,20 @@ def gps_scan_save(request):
     except Exception as e:
         # Log the error if needed
         return JsonResponse({"status": "error", "message": str(e), "traceback": traceback.format_exc()})
+    
+def veld_gps_saving(request):
+    if request.method == 'POST':
+        try:
+            veld = request.POST['veld']
+            gps_locations = request.POST['gps_locations']
+            
+            veld_object = Veld.objects.create(id=veld)
+
+            for gps_location in gps_locations:
+                VeldGps.objects.create(veld=veld_object, lat=gps_location['lat'], lng=gps_location['lng'])
+            
+            return JsonResponse({"status": "success"})
+        
+        except Exception as e:
+            # Log the error if needed
+            return JsonResponse({"status": "error", "message": str(e), "traceback": traceback.format_exc()})
